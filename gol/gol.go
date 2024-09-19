42d5ee700979928021cad3d2e137a5935ad6f3f1
onett <onettperera@gmail.com>
2024-09-19 14:39:12 +0100

    start of game of life

--- gol/gol.go
+++ gol/gol.go
@@ -1,6 +1,32 @@
 package main
 
 func calculateNextState(p golParams, world [][]byte) [][]byte {
+	for j := 0; j < p.imageHeight; j++ {
+		for i := 0; i < p.imageWidth; i++ {
+			sum := 0
+			if world[i%15][j%15] == 255 {
+				sum += 1
+			}
+			if world[(i+1)%15][j%15] == 255 {
+				sum += 1
+			}
+			if world[(i-1)%15][j%15] == 255 {
+				sum += 1
+			}
+			if world[(i-1)%15][(j+1)%15] == 255 {
+				sum += 1
+			}
+			if world[(i-1)%15][(j-1)%15] == 255 {
+				sum += 1
+			}
+			if world[(i+1)%15][(j+1)%15] == 255 {
+				sum += 1
+			}
+			if world[(i+1)%15][(j-1)%15] == 255 {
+				sum += 1
+			}
+		}
+	}
 	return world
 }
 
